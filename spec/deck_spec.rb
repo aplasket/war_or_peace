df76a035fa84a16fdc48969fb5f934e47b050699
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-22 20:27:08 -0400

    Clean up rspec describe blocks

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,7 +3,7 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
-  it "exists" do
+  it 'exists' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
@@ -13,28 +13,30 @@ RSpec.describe Deck do
     expect(deck).to be_an_instance_of(Deck)
   end
 
-  it "has a card" do
+  it 'has readable attributes' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
-    expect(deck.cards).to eq(cards)
+    expect(deck.cards).to be_an(Array)
+    expect(deck.cards).to eq([card1, card2, card3])
   end
 
-  it "ranks cards at index position" do
+  it 'can get rank of card at specific index position' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
+    expect(deck.cards).to eq([card1, card2, card3])
     expect(deck.rank_of_card_at(0)).to eq(12) 
     expect(deck.rank_of_card_at(2)).to eq(14)   
   end
 
-  it "returns array of high ranking cards" do
+  it 'returns array of high ranking cards' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
@@ -44,7 +46,7 @@ RSpec.describe Deck do
     expect(deck.high_ranking_cards).to eq([card1, card3])
   end
 
-  it "returns percentage of cards that are high ranking" do
+  it 'returns percentage of cards that are high ranking' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
@@ -54,7 +56,7 @@ RSpec.describe Deck do
     expect(deck.percent_high_ranking).to eq(66.67)
   end
 
-  it "removes top card from deck" do
+  it 'removes top card from deck' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
@@ -67,7 +69,7 @@ RSpec.describe Deck do
     expect(deck.percent_high_ranking).to eq(50)
   end
 
-  it "adds a card to bottom of deck" do
+  it 'adds a card to bottom of deck' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)
